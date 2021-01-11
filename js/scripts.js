72dd95b11230c14b491d9e97feaa78d8360d4ffb
diegosalazar343 <diegosalazar343@gmail.com>
2021-01-11 13:43:59 -0800

    add subtract, multiply, and division to calculator logic
    Co-authored-by: Stormy Starkey <stormy.starkey@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
-function add (number1, number2) {
-  return number1 + number2;
+//function add (number1, number2) {
+  //return number1 + number2;
+//}
+
+function subtract(number1, number2) {
+  return number1 - number2;
 }
+
+//function mulitply (number1, number2) {
+  //return number1 * number2;
+//}
+
+//function divide (number1, number2) {
+  //return number1 / number2;
+//}
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
+//const result = add(number1, number2);
+const result = subtract(number1, number2);
+//const result = multiply(number1, number2);
+//const result = divide(number1, number2);
 alert(result);
